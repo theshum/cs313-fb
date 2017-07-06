7e895b87fe475aa5ff5cb5165576b730725018d5
gishido <theshum@gmail.com>
2017-07-05 23:52:32 -0700

    working through tutorial for passport and facebook auth https://www.youtube.com/watch?v=OMcWgmkMpEE

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '244775159354807',
+		'clientSecret': '255c91a84c3bd8cfed91c7f3ef6dbc9a',
+		'callbackURL': 'http://localhost/3000/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
