5126a35618df4a171f9c0f716ec17f98bed0703e
gishido <theshum@gmail.com>
2017-07-06 01:08:25 -0700

    installed passport and required it

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
