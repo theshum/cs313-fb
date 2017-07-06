bca998fc13c25ea708ac58521dc91cdd73beb9ad
gishido <theshum@gmail.com>
2017-07-06 00:58:02 -0700

    had to add node-gyp, install python 2.y, and bcrypt

--- routes/facebook.js
+++ routes/facebook.js
@@ -1,16 +1,21 @@
-// Redirect the user to Facebook for authentication.  When complete,
-// Facebook will redirect the user back to the application at
-//     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook'));
+var User = require('../config/user');
 
-// Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-/*app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/login' }));*/
+module.exports = function(app, passport)
+{
+    // Redirect the user to Facebook for authentication.  When complete,
+    // Facebook will redirect the user back to the application at
+    //     /auth/facebook/callback
+    app.get('/auth/facebook', passport.authenticate('facebook'));
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/profile',
-                                      failureRedirect: '/' }));
+    // Facebook will redirect the user to this URL after approval.  Finish the
+    // authentication process by attempting to obtain an access token.  If
+    // access was granted, the user will be logged in.  Otherwise,
+    // authentication has failed.
+    /*app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/',
+                                        failureRedirect: '/login' }));*/
+
+    app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/profile',
+                                        failureRedirect: '/' }));
+};
